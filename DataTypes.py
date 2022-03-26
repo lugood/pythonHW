0facaa02594d231328eb2690ebdd640e2e2fc21c
Louis Lee <lugood@gmail.com>
2022-03-27 01:16:47 +0800

    add 3 files DataTypes.py, Declare.py, input.py on 2022/03/27

---
+++ DataTypes.py
@@ -0,0 +1,26 @@
+"""
+data types (資料型別)
+1. 整數 integer (int)，沒有小數點的數字
+2. 浮點數 float (float)，有小數點的數字
+3. 布林值 boolean (bool),
+   True表真的、正確的、對的；False表假的、錯誤的、錯的
+4. 字串 string (str) 空格或任何文字 
+   '', 'Louis', ''....
+"""
+
+# 整數 int
+price = 10
+print(' price = ' + str(price) )
+
+# 浮點數 float
+pen = 0.38
+print(" pen = " + str(pen) )
+
+# 布林值 bool
+rain = True
+print(" rain = ") 
+print(rain )
+
+# 字串 str
+name = 'Louis' 
+print(" name = " + str(name) )
---
+++ Declare.py
@@ -0,0 +1,14 @@
+x = 5
+print(" x = " + str(x) )
+
+x = 10
+print(" x = " + str(x) )
+
+x = 1000
+print(" x = " + str(x) )
+
+y = 2 
+print(" y = " + str(y) )
+
+y = 2000 
+print(" y = " + str(y) )
